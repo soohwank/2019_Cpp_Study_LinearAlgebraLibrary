119d1551426a9b8b5e5237bac3b037989b2db6a6
JaeWoo LEE <ljw5641@naver.com>
2019-07-01 18:26:06 +0900

    MatrixXd(07.01)

--- src/main.cpp
+++ src/main.cpp
@@ -1,631 +1,103 @@
-#include <iostream>
+#include "vector.hpp"
+#include "matrix.hpp"
+using namespace lal;
 using namespace std;
-
-class Matrix3d
-{
-public:
-	// default constructor
-	Matrix3d()
-	{
-		// zero initialization
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				num[i][j] = 0.0;
-			}
-		}
-	}
-
-	// constructor
-	Matrix3d(const double num1, const double num2, const double num3,
-		const double num4, const double num5, const double num6,
-		const double num7, const double num8, const double num9)
-	{
-		num[0][0] = num1;
-		num[0][1] = num2;
-		num[0][2] = num3;
-
-		num[1][0] = num4;
-		num[1][1] = num5;
-		num[1][2] = num6;
-
-		num[2][0] = num7;
-		num[2][1] = num8;
-		num[2][2] = num9;
-	}
-
-	// constructor
-	Matrix3d(const Matrix3d &m)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				num[i][j] = m.num[i][j];
-			}
-		}
-	}
-
-	// print function
-	void print() const
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				cout << num[i][j] << ' ';
-			}
-			cout << endl;
-		}
-	}
-
-	// matrix addition
-	Matrix3d add(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] + m.num[i][j];
-			}
-		}
-		return result;
-	}
-
-	// matrix subtraction
-	Matrix3d subtract(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] - m.num[i][j];
-			}
-		}
-		return result;
-	}
-
-	// matrix multiplication
-	Matrix3d multiply(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] * m.num[i][j];
-			}
-		}
-		return result;
-	}
-
-private:
-	double num[3][3];
-	friend class Vector3d;
-};
-
-class Vector3d
-{
-public:
-	// default constructor
-	Vector3d()
-	{
-		// zero initialization
-		for (int i = 0; i < 3; i++)
-		{
-			data[i] = 0.0;
-		}
-	}
-
-	// constructor
-	Vector3d(const double x, const double y, const double z)
-	{
-		data[0] = x;
-		data[1] = y;
-		data[2] = z;
-	}
-
-	// constructor
-	Vector3d(const Vector3d &v)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			data[i] = v.data[i];
-		}
-	}
-
-	// print function
-	void print() const
-	{
-		cout << "[" << data[0] << ", "
-			<< data[1] << ", "
-			<< data[2] << "]" << endl;
-	}
-
-	// add
-	Vector3d add(const Vector3d &v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] + v.data[i];
-		}
-		return result;
-	}
-
-	Vector3d subtract(const Vector3d &v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] - v.data[i];
-		}
-		return result;
-	}
-
-	double dotProduct(const Vector3d &v) const
-	{
-		double result = 0.0;
-		for (int i = 0; i < 3; i++)
-		{
-			result += data[i] * v.data[i];
-		}
-		return result;
-	}
-
-	Vector3d crossProduct(const Vector3d &v) const
-	{
-		Vector3d result;
-
-		result.data[0] = data[1] * v.data[2] - data[2] * v.data[1];
-		result.data[1] = data[0] * v.data[2] - data[2] * v.data[0];
-		result.data[2] = data[0] * v.data[1] - data[1] * v.data[0];
-		return result;
-	}
-
-	// scalar addition
-	Vector3d add(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] + v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// scalar subtract
-	Vector3d subtract(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] - v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// scalar multiply
-	Vector3d multiply(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] * v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// matrix-vector multiplication
-	Vector3d multiply(const Vector3d &v) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] * v.data[i];
-			}
-		}
-		return result;
-	}
-
-	// getter
-	double x() const
-	{
-		return data[0];
-	}
-
-	// setter
-	double& x()
-	{
-		return data[0];
-	}
-
-private:
-	// member variables
-	double data[3];
-
-public:
-	// static
-	const static int dim = 3;
-};
-
 
 int main()
 {
+	//////////////////////////////////////////
+	// vector
+	/////////////////////////////////////////
 	Vector3d v1(1.0, 2.0, 3.0);
-	//v1.print();
+	v1.print();
 
 	Vector3d v2(-2.0, 1.0, 2.0);
+	v2.print();
 
-	/*
-	// add
-	Vector3d v3 = v1.add(v2);
+	// addition
+	(v1.add(v2)).print();
+	(v1.add(1.0)).print();
 
-	// subtract
-	Vector3d v4 = v1.subtract(v2);
+	// subtracttion
+	(v1.subtract(v2)).print();
+	(v1.subtract(1.0)).print();
+
+	// multiplication
+	(v1.multiply(2.0)).print();
 
 	// dot product
-	double dotProduct = v1.dotProduct(v2);
+	cout << v1.dotProduct(v2) << endl;
 
 	// cross product
-	Vector3d v4 = v1.crossProduct(v2);
-	*/
+	(v1.crossProduct(v2)).print();
 
-	// scalar addition
-	Vector3d v5 = v1.add(3.0);
-
-	// scalar subtraction
-	Vector3d v6 = v1.subtract(3.0);
-
-	// scalar multiplication
-	Vector3d v7 = v1.multiply(3.0);
+	//////////////////////////////////////////
+	// matrix 3d
+	/////////////////////////////////////////
 
 	// matrix
 	Matrix3d m1(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
 	m1.print();
+
 	Matrix3d m2(-1.0, -2.0, -3.0, -4.0, -5.0, -6.0, 7.0, 8.0, 9.0);
 	m2.print();
+	
+	// addition
+	(m1.add(m2)).print();
+	(m1.add(1.0)).print();
 
-	// matrix addition
-	Matrix3d m3 = m1.add(m2);
+	// subtraction
+	(m1.subtract(m2)).print();
+	(m1.subtract(1.0)).print();
 
-	// matrix subtraction
-	Matrix3d m4 = m1.subtract(m2);
+	// multiplication
+	(m1.multiply(m2)).print();
+	(m1.multiply(v1)).print();
+	(m1.multiply(2.0)).print();
 
-	// matrix multiplication
-	Matrix3d m5 = m1.multiply(m2);
+	// inverse
+	//(m1.inverse()).print();
 
-	// matrix-vector multiplication
-	Vector3d v8 = m1.multiply(v1);
+	//////////////////////////////////////////
+	// matrix 
+	/////////////////////////////////////////
 
-	// matrix inverse
-	//Matrix3d m2 = m1.inverse();
-
-	return 0;
-}#include <iostream>
-using namespace std;
-
-class Matrix3d
-{
-public:
-	// default constructor
-	Matrix3d()
+	MatrixXd m3(4, 3);
+	for (size_t row = 0; row < 4; row++)
 	{
-		// zero initialization
-		for (int i = 0; i < 3; i++)
+		for (size_t col = 0; col < 3; col++)
 		{
-			for (int j = 0; j < 3; j++)
-			{
-				num[i][j] = 0.0;
-			}
+			m3.set(row, col,row + col);
 		}
 	}
 
-	// constructor
-	Matrix3d(const double num1, const double num2, const double num3,
-		const double num4, const double num5, const double num6,
-		const double num7, const double num8, const double num9)
+	MatrixXd m4(4, 3);
+	for (size_t row = 0; row < 4; row++)
 	{
-		num[0][0] = num1;
-		num[0][1] = num2;
-		num[0][2] = num3;
-
-		num[1][0] = num4;
-		num[1][1] = num5;
-		num[1][2] = num6;
-
-		num[2][0] = num7;
-		num[2][1] = num8;
-		num[2][2] = num9;
-	}
-
-	// constructor
-	Matrix3d(const Matrix3d &m)
-	{
-		for (int i = 0; i < 3; i++)
+		for (size_t col = 0; col < 3; col++)
 		{
-			for (int j = 0; j < 3; j++)
-			{
-				num[i][j] = m.num[i][j];
-			}
+			m4.set(row, col,row * 2);
 		}
 	}
 
-	// print function
-	void print() const
+	MatrixXd m5(3, 5);
+	for (size_t row = 0; row < 3; row++)
 	{
-		for (int i = 0; i < 3; i++)
+		for (size_t col = 0; col < 5; col++)
 		{
-			for (int j = 0; j < 3; j++)
-			{
-				cout << num[i][j] << ' ';
-			}
-			cout << endl;
+			m5.set(row, col, 2.0);
 		}
 	}
 
-	// matrix addition
-	Matrix3d add(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] + m.num[i][j];
-			}
-		}
-		return result;
-	}
+	(m3.add(2.0)).print();
+	(m3.add(m4)).print();
+	m3.print();
+	(m3.subtract(m4)).print();
+	(m3.subtract(3.0)).print();
 
-	// matrix subtraction
-	Matrix3d subtract(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] - m.num[i][j];
-			}
-		}
-		return result;
-	}
-
-	// matrix multiplication
-	Matrix3d multiply(const Matrix3d &m) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] * m.num[i][j];
-			}
-		}
-		return result;
-	}
-
-private:
-	double num[3][3];
-	friend class Vector3d;
-};
-
-class Vector3d
-{
-public:
-	// default constructor
-	Vector3d()
-	{
-		// zero initialization
-		for (int i = 0; i < 3; i++)
-		{
-			data[i] = 0.0;
-		}
-	}
-
-	// constructor
-	Vector3d(const double x, const double y, const double z)
-	{
-		data[0] = x;
-		data[1] = y;
-		data[2] = z;
-	}
-
-	// constructor
-	Vector3d(const Vector3d &v)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			data[i] = v.data[i];
-		}
-	}
-
-	// print function
-	void print() const
-	{
-		cout << "[" << data[0] << ", "
-			<< data[1] << ", "
-			<< data[2] << "]" << endl;
-	}
-
-	// add
-	Vector3d add(const Vector3d &v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] + v.data[i];
-		}
-		return result;
-	}
-
-	Vector3d subtract(const Vector3d &v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] - v.data[i];
-		}
-		return result;
-	}
-
-	double dotProduct(const Vector3d &v) const
-	{
-		double result = 0.0;
-		for (int i = 0; i < 3; i++)
-		{
-			result += data[i] * v.data[i];
-		}
-		return result;
-	}
-
-	Vector3d crossProduct(const Vector3d &v) const
-	{
-		Vector3d result;
-
-		result.data[0] = data[1] * v.data[2] - data[2] * v.data[1];
-		result.data[1] = data[0] * v.data[2] - data[2] * v.data[0];
-		result.data[2] = data[0] * v.data[1] - data[1] * v.data[0];
-		return result;
-	}
-
-	// scalar addition
-	Vector3d add(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] + v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// scalar subtract
-	Vector3d subtract(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] - v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// scalar multiply
-	Vector3d multiply(const double v) const
-	{
-		Vector3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			result.data[i] = data[i] * v;
-			cout << result.data[i] << ' ';
-		}
-		cout << endl;
-		return result;
-	}
-
-	// matrix-vector multiplication
-	Vector3d multiply(const Vector3d &v) const
-	{
-		Matrix3d result;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				result.num[i][j] = num[i][j] * v.data[i];
-			}
-		}
-		return result;
-	}
-
-	// getter
-	double x() const
-	{
-		return data[0];
-	}
-
-	// setter
-	double& x()
-	{
-		return data[0];
-	}
-
-private:
-	// member variables
-	double data[3];
-
-public:
-	// static
-	const static int dim = 3;
-};
-
-
-int main()
-{
-	Vector3d v1(1.0, 2.0, 3.0);
-	//v1.print();
-
-	Vector3d v2(-2.0, 1.0, 2.0);
-
-	/*
-	// add
-	Vector3d v3 = v1.add(v2);
-
-	// subtract
-	Vector3d v4 = v1.subtract(v2);
-
-	// dot product
-	double dotProduct = v1.dotProduct(v2);
-
-	// cross product
-	Vector3d v4 = v1.crossProduct(v2);
-	*/
-
-	// scalar addition
-	Vector3d v5 = v1.add(3.0);
-
-	// scalar subtraction
-	Vector3d v6 = v1.subtract(3.0);
-
-	// scalar multiplication
-	Vector3d v7 = v1.multiply(3.0);
-
-	// matrix
-	Matrix3d m1(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
-	m1.print();
-	Matrix3d m2(-1.0, -2.0, -3.0, -4.0, -5.0, -6.0, 7.0, 8.0, 9.0);
-	m2.print();
-
-	// matrix addition
-	Matrix3d m3 = m1.add(m2);
-
-	// matrix subtraction
-	Matrix3d m4 = m1.subtract(m2);
-
-	// matrix multiplication
-	Matrix3d m5 = m1.multiply(m2);
-
-	// matrix-vector multiplication
-	Vector3d v8 = m1.multiply(v1);
-
-	// matrix inverse
-	//Matrix3d m2 = m1.inverse();
-
+	(m3.multiply(m5)).print();
+	(m3.multiply(v1)).print();
+	(m3.multiply(2.0)).print();
+	
 	return 0;
 }
---
+++ src/matrix.hpp
@@ -0,0 +1,247 @@
+#ifndef _MATRIX3D_HPP_
+#define _MATRIX3D_HPP_
+
+#include <iostream>
+#include <cassert>
+#include "vector.hpp"
+
+namespace lal
+{
+	//////////////////////////////////////////
+	// class MatrixXd 
+	/////////////////////////////////////////
+	class MatrixXd
+	{
+	public:
+		// constructor
+		MatrixXd(const size_t rows, const size_t cols)
+			: m_rows(rows), m_cols(cols)
+		{
+			m_ppData = new double*[m_rows];
+			for (size_t i = 0; i < m_rows; i++)
+			{
+				m_ppData[i] = new double[m_cols];
+				for (size_t j = 0; j < m_cols; j++)
+				{
+					m_ppData[i][j] = 0.0;
+				}
+			}
+		}
+
+		// copy constructor
+		MatrixXd(const MatrixXd &m)
+			: MatrixXd(m.m_rows, m.m_cols)
+		{
+			// speicial case
+			assert(m_rows == m.m_rows && m_cols == m.m_cols);
+
+			for (int row = 0; row < m_rows; row++)
+			{
+				for (int col = 0; col < m_cols; col++)
+				{
+					m_ppData[row][col] = m.m_ppData[row][col];
+				}
+			}
+		}
+
+		// destructor
+		~MatrixXd()
+		{
+			for (size_t i = 0; i < m_rows; i++)
+			{
+				delete[]  m_ppData[i];
+			}
+			delete[] m_ppData;
+		}
+
+		// addition
+		MatrixXd add(const MatrixXd &m) const
+		{
+			// speicial case
+			assert(m_rows == m.m_rows && m_cols == m.m_cols);
+
+			MatrixXd result(m_rows, m_cols);
+			for (size_t row = 0; row < m_rows; row++)
+			{
+				for (size_t col = 0; col < m_cols; col++)
+				{
+					result.m_ppData[row][col] = m_ppData[row][col] + m.m_ppData[row][col];
+				}
+			}
+			return result;
+		}
+
+		MatrixXd add(const double s) const
+		{
+			// speicial case
+			
+			MatrixXd result(m_rows, m_cols);
+			for (size_t row = 0; row < m_rows; row++)
+			{
+				for (size_t col = 0; col < m_cols; col++)
+				{
+					result.m_ppData[row][col] = m_ppData[row][col] + s;
+				}
+			}
+			return result;
+		}
+
+		// subtraction
+		MatrixXd subtract(const MatrixXd &m) const
+		{
+			// speicial case
+			assert(m_rows == m.m_rows && m_cols == m.m_cols);
+
+			MatrixXd result(m_rows, m_cols);
+			for (size_t row = 0; row < m_rows; row++)
+			{
+				for (size_t col = 0; col < m_cols; col++)
+				{
+					result.m_ppData[row][col] = m_ppData[row][col] - m.m_ppData[row][col];
+				}
+			}
+			return result;
+		}
+
+		MatrixXd subtract(const double s) const
+		{
+			// speicial case
+			assert(m_rows == m_rows && m_cols == m_cols);
+
+			MatrixXd result(m_rows, m_cols);
+			for (size_t row = 0; row < m_rows; row++)
+			{
+				for (size_t col = 0; col < m_cols; col++)
+				{
+					result.m_ppData[row][col] = m_ppData[row][col] - s;
+				}
+			}
+			return result;
+		}
+
+		// multiplication
+		MatrixXd multiply(const MatrixXd &m) const
+		{
+			// speicial case
+			assert(m_cols == m.m_rows);
+
+			MatrixXd result(m_rows, m.m_cols);
+			for (int row = 0; row < m_rows; row++)
+			{
+				for (int col = 0; col < m.m_cols; col++)
+				{
+					result.m_ppData[row][col] = 0.0;
+					for (int i = 0; i < m_cols; i++)
+					{
+						result.m_ppData[row][col] += m_ppData[row][i] * m.m_ppData[i][col];
+					}
+				}
+			}
+			return result;
+		}
+
+		Vector3d multiply(const Vector3d &v) const
+		{
+			Vector3d result;
+			for (int row = 0; row < 3; row++)
+			{
+				result.data[row] = 0.0;
+				for (int i = 0; i < 3; i++)
+				{
+					result.data[row] += m_ppData[row][i] * v.data[i];
+				}
+			}
+			return result;
+		}
+
+		MatrixXd multiply(const double s) const
+		{
+			MatrixXd result(m_rows, m_cols);
+			for (int row = 0; row < m_rows; row++)
+			{
+				for (int col = 0; col < m_cols; col++)
+				{
+					result.m_ppData[row][col] = m_ppData[row][col] * s;
+				}
+			}
+			return result;
+		}
+
+		void set(const size_t row, const size_t col, const double value)
+		{
+			m_ppData[row][col] = value;
+		}
+
+		// print function
+		void print() const
+		{
+			std::cout << "[";
+			for (int row = 0; row < m_rows; row++)
+			{
+				for (int col = 0; col < m_cols; col++)
+				{
+					std::cout << m_ppData[row][col];
+					if (col != (m_cols - 1))std::cout << ", ";
+				}
+				if (row != (m_rows - 1))std::cout << std::endl;
+			}
+			std::cout << "]" << std::endl;
+		}
+
+	private:
+		MatrixXd()
+			: m_rows(0), m_cols(0)
+		{
+		}
+
+	protected:
+		// size
+		const size_t m_rows;
+		const size_t m_cols;
+
+		// data
+		double **m_ppData;
+	};
+
+	//////////////////////////////////////////
+	// class Matrix3d 
+	/////////////////////////////////////////
+	class Matrix3d : public MatrixXd
+	{
+	public:
+		// default constructor
+		Matrix3d()
+			: MatrixXd(3,3)
+		{
+
+		}
+
+		// constructor
+		Matrix3d(const double m00, const double m01, const double m02,
+			     const double m10, const double m11, const double m12,
+			     const double m20, const double m21, const double m22)
+			: MatrixXd(3, 3)
+		{
+			m_ppData[0][0] = m00; m_ppData[0][1] = m01; m_ppData[0][2] = m02;
+			m_ppData[1][0] = m10; m_ppData[1][1] = m11; m_ppData[1][2] = m12;
+			m_ppData[2][0] = m20; m_ppData[2][1] = m21; m_ppData[2][2] = m22;
+		}
+
+		// copy constructor
+		Matrix3d(const Matrix3d &m)
+			: MatrixXd(m)
+		{
+
+		}
+
+	protected:
+		// member variables
+		double data[3][3];
+
+	public:
+		// static
+		const static int dim = 3;
+	};
+
+} // namespace lal
+#endif // _MATRIX3D_HPP_
